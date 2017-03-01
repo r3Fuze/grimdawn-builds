467d7a87559ad0a424ef281809545ff73b320ccb
Morten Lindhardt <r3fuze@gmail.com>
2017-03-01 01:13:41 +0100

    Starting tag parsing

--- parse-builds.js
+++ parse-builds.js
@@ -1,5 +1,20 @@
 let buildData = require("./builds.json")
 
+const TAGS = {
+    melee: "melee",
+    ranged: "ranged",
+    caster: "caster",
+    pet: "pet",
+    hardcore: "hc",
+    ultimate: "u",
+    crucible: "c",
+    gladiator: "c+",
+    download: "d",
+    video: "vid",
+    gear: "g[1-5]",
+    leveling: "l"
+}
+
 const testBuild = {
     "name": "Deathmarked Blademaster Multi-Proc",
     "data": {
@@ -30,5 +45,25 @@ const testBuild = {
     "description": "[Melee] [v1.0.0.7.] Deathmarked Blademaster Multi-Proc (Superfluff)"
 }
 
+const isNumeric = str => !isNaN(str)
+
 console.log(testBuild.name)
 console.log(`  Damage types: ${testBuild.data.Damage.join(", ")}`)
+
+let gameVersion = new RegExp(/\[v?((?:\d\.?){1,4}.?)\]/g).exec(testBuild.description)
+let numericVersion = -1
+
+if (gameVersion === null) {
+    console.log("!!! version string not found")
+} else {
+    gameVersion = gameVersion[1].replace(/.$/gm, "")
+    console.log("Version: " + gameVersion)
+
+    let numericReplace = gameVersion.replace(/\./g, "")
+
+    if (isNumeric(numericReplace)) {
+        numericVersion = Number(numericReplace)
+
+        console.log("Numeric version: " + numericVersion)
+    }
+}
